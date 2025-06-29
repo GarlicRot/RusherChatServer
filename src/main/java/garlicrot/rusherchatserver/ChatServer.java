123f3c15dbd17fb7737ebd12dada3da014933f3d
GarlicRot <104709774+GarlicRot@users.noreply.github.com>
2025-06-29 16:44:16 -0700

    refactor(server): unify colored message broadcast and remove sender-specific formatting

--- src/main/java/garlicrot/rusherchatserver/ChatServer.java
+++ src/main/java/garlicrot/rusherchatserver/ChatServer.java
@@ -160,19 +160,11 @@ public class ChatServer extends WebSocketServer {
             Message colored = new Message(username, content, coloredUsername);
             String json = gson.toJson(colored);
 
-            Message selfMsg = new Message(username, content, "[You]");
-            String jsonSelf = gson.toJson(selfMsg);
-
             for (WebSocket client : clients) {
                 if (client.isOpen()) {
-                    if (client == conn) {
-                        client.send(jsonSelf);
-                    } else {
-                        client.send(json);
-                    }
+                    client.send(json); // Send same colored message to everyone
                 }
             }
-
 
             LOGGER.fine("Message from " + username + ": " + content);
         } catch (Exception e) {
